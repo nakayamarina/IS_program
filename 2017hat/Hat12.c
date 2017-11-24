9959266f42aadae28a966473f9c52964cfe345a2
中山 梨奈 <nakayamarina@chuuzan-no-iMac-6.local>
2017-11-24 17:11:36 +0900

    Correction:Hat12.c

--- 2017hat/Hat12.c
+++ 2017hat/Hat12.c
@@ -18,17 +18,17 @@ int main(int argc, char *argv[]) {
   //文字列の長さ + '年''月''日' で11回繰り返す
   for(i = 0; i < 11; i++) {
     if(i < 4) {
-      printf("%c", argv[1][count]);
+      printf("%c", *(argv[1]+count));
       count += 1;
     } else if(i == 4) {
       printf("年");
     } else if((5 <= i) && (i < 7)) {
-      printf("%c", argv[1][count]);
+      printf("%c", *(argv[1]+count));
       count += 1;
     } else if(i == 7) {
       printf("月");
     } else if((8 <= i) && (i < 10)){
-      printf("%c", argv[1][count]);
+      printf("%c", *(argv[1]+count));
       count += 1;
     } else if(i == 10){
       printf("日\n");
